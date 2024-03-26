18294a7d858ac83bd08f032cae0d3b8f5ffd5bb8
rajkonkret <rajkonkret660@gmail.com>
2024-03-26 12:08:34 +0100

    1 add generatory3

--- generatory2.py
+++ generatory2.py
@@ -55,7 +55,61 @@ print(next(fi))
 print(next(fi))
 print(next(fi))
 print(sys.int_info)
+
+
 # sys.int_info(bits_per_digit=30
 #              , sizeof_digit=4,
 #              default_max_str_digits=4300,
 #              str_digits_check_threshold=640)
+
+def fibo_with_index(n):
+    a, b = 0, 1
+    for ind in range(n):
+        yield ind, a
+        a, b = b, a + b
+
+
+fibo2 = fibo_with_index(10)
+print(next(fibo2))
+print(next(fibo2))
+print(next(fibo2))
+print(next(fibo2))
+print(next(fibo2))  # (4, 3)
+
+for i in fibo_with_index(10):
+    print(f"Element {i}")  # f-string, w {} mozemy wstawiac wartośc np.: zmiennej
+# Element (8, 21)
+a, b = (8, 21)
+print(a)
+print(b)
+
+for i, n in fibo_with_index(10):
+    print(f"Element {i}, wartość {n}")  # Element 9, wartość 34
+
+a, *b = 1, 2, 3
+print(a)  # 1
+print(b)  # [2, 3]
+
+
+def counter(start=0):
+    n = start
+    while True:
+        result = yield n
+        print(result)
+        n += 1
+        if result == 'q':
+            break
+
+
+c = counter(10)
+print(next(c))
+print(next(c))
+print(next(c))
+print(c.send("OK"))  # OK
+try:
+    print(c.send("q"))
+except StopIteration as e:
+    print("Generator zakończył działanie", e)
+# print(next(c))  #
+d = counter(10)
+print(next(d))
