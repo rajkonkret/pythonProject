14b1225dd4e69568e85e4387cb8a283f99f34e7b
rajkonkret <rajkonkret660@gmail.com>
2024-03-26 13:36:49 +0100

    1 update dekoratory.py

--- dekoratory.py
+++ dekoratory.py
@@ -10,7 +10,25 @@ def fun1():
     return fwew  # zwracamy adres funkcji, referencje
 
 
+def dekor(funk):
+    def wew():
+        print("Dekorujemy")
+        return funk()
+
+    return wew
+
+
 print(fun1())  # <function fun1.<locals>.fwew at 0x0000010D736098A0>
 xFun = fun1()
 print(type(xFun))  # <class 'function'>
 xFun(4, 5)
+
+
+@dekor
+def hej():
+    print("Hej!!!")
+
+
+hej()
+# Dekorujemy
+# Hej!!!
