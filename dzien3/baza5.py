0ab79d0191171e54b0053335bbcfc9f33a344a5b
rajkonkret <rajkonkret660@gmail.com>
2024-03-28 12:41:23 +0100

    3 add baza6

--- dzien3/baza5.py
+++ dzien3/baza5.py
@@ -1,2 +1,35 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, create_engine
 from sqlalchemy.orm import relationship, sessionmaker, declarative_base
+
+engine = create_engine('sqlite:///book_shop.db')  # baza danych w pamięci
+Base = declarative_base()
+
+
+class Author(Base):
+    __tablename__ = 'authors'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+    books = relationship('Book', back_populates='author')
+
+
+class Publisher(Base):
+    __tablename__ = 'publishers'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+    books = relationship('Book', back_populates='publisher')
+
+
+class Book(Base):
+    __tablename__ = 'books'
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    author_id = Column(Integer, ForeignKey('authors.id'))
+    publisher_id = Column(Integer, ForeignKey('publishers.id'))
+
+    author = relationship("Author", back_populates='books')
+    publisher = relationship('Publisher', back_populates='books')
+
+
+Base.metadata.create_all(engine)
