e7fc278f6969a25024888a903e9af60e81771fb6
rajkonkret <rajkonkret660@gmail.com>
2024-03-28 11:45:14 +0100

    3 update baza3 sqlalchemy

--- dzien3/baza3-sqlalchemy.py
+++ dzien3/baza3-sqlalchemy.py
@@ -12,6 +12,31 @@ class User(Base):
     name = Column(String)
     age = Column(Integer)
 
+    def __repr__(self):
+        return f"{self.id} {self.name}"
 
-engine = create_engine('sqlite:///my_database_alch.db')
+
+# echo=True - dodatkowe logi
+engine = create_engine('sqlite:///my_database_alch.db', echo=True)
 Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+sesion = Session()
+
+new_uer = User(name="Jan Kowalski", age=30)
+sesion.add(new_uer)  # INSERT INTO users (name, age) VALUES (?, ?)
+
+sesion.commit()
+sesion.close()
+
+users = sesion.query(User).all()
+for user in users:
+    print(user)
+    # print(user.name)
+# Po dodaniu __repr__
+# 1 Jan Kowalski
+# 2 Jan Kowalski
+# 3 Jan Kowalski
+# 4 Jan Kowalski
+# 5 Jan Kowalski
+# 6 Jan Kowalski
